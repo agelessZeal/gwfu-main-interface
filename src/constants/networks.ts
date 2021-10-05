49f3cee8ea2cc3ae6e9c65ed71ff0a21e0df475b
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-10-05 20:54:13 +0300

    gwf chain image with gold

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -17,7 +17,7 @@ const Rinkeby = '/images/networks/rinkeby-network.jpg'
 const Ropsten = '/images/networks/ropsten-network.jpg'
 const xDai = '/images/networks/xdai-network.jpg'
 const Celo = '/images/networks/celo-network.jpg'
-const GWFU = '/images/networks/gwf_network.png'
+const GWFU = '/images/networks/gwfu.png'
 const Palm = 'https://raw.githubusercontent.com/sushiswap/icons/master/network/palm.jpg'
 
 export const NETWORK_ICON = {
