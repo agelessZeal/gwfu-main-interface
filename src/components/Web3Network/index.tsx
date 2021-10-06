44db0c7bb7b69d3ae82a51600b13d1f7b7a60928
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-10-06 03:35:01 +0300

    remove network change

--- src/components/Web3Network/index.tsx
+++ src/components/Web3Network/index.tsx
@@ -16,7 +16,7 @@ function Web3Network(): JSX.Element | null {
   return (
     <div
       className="flex items-center rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto"
-      onClick={() => toggleNetworkModal()}
+      // onClick={() => toggleNetworkModal()}
     >
       <div className="grid items-center grid-flow-col px-3 py-2 space-x-2 text-sm rounded-lg pointer-events-auto auto-cols-max bg-dark-1000 text-secondary">
         <Image src={NETWORK_ICON[chainId]} alt="Switch Network" className="rounded-md" width="22px" height="22px" />
